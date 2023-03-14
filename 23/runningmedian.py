a5811bfec50ecf4c3f0a6fe059dd73e6351b7ccb
zzeuui <jyj970328@gmail.com>
2023-03-14 19:33:28 +0900

    12th study

--- 23/runningmedian.py
+++ 23/runningmedian.py
@@ -1,6 +1,6 @@
 #bisect: 점프투파이썬 - https://wikidocs.net/105425
 #bisect document: https://docs.python.org/ko/3/library/bisect.html
-
+"""
 from sys import stdin
 from bisect import insort_left
 
@@ -15,35 +15,36 @@ if __name__=='__main__':
             temp = (temp * a + b) % 20090711
             insort_left(A, temp) #이진 분할 알고리즘으로 정렬된 리스트를 구성
         print(ans%20090711)
-
+"""
 import heapq
 
 def main():
     for _ in range(int(input().strip())):
         N, a, b = map(int, input().strip().split())
         cur = 1983
-        nums = [cur]
         ans = 0
         
-        max_heap = [-cur]
-        heapq.heapify(max_heap)
+        #the heapq is for min heap,
+        #so need to change the sign of the element in max heap
+        max_heap = []
         min_heap = [] 
-        heapq.heapify(min_heap)
-        for i in range(1, N):
-            cur = (cur * a + b) % 20090711
+
+        for i in range(N):
             if(len(max_heap) > len(min_heap)):
                 heapq.heappush(min_heap, cur)
             else:
                 heapq.heappush(max_heap, -cur)
-            if len(min_heap) > 0  and -max_heap[0] > min_heap[0]:
-                t1 = -heapq.heappop(min_heap)
-                t2 = -heapq.heappop(max_heap)
-                heapq.heappush(min_heap, t2)
-                heapq.heappush(max_heap, t1)
+
+            if max_heap and min_heap and -max_heap[0] > min_heap[0]:
+                t1 = heapq.heappop(min_heap)
+                t2 = heapq.heappop(max_heap)
+                heapq.heappush(min_heap, -t2)
+                heapq.heappush(max_heap, -t1)
+
             ans += -max_heap[0]
-                
-        print((ans + 1983) % 20090711)
-"""
+            cur = (cur * a + b) % 20090711
+
+        print((ans) % 20090711)
+
 if __name__ == '__main__':
     main()
-"""
