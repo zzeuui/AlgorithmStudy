c5da8e419a48fad303317d5dd0ae028dd8b2b1ad
zzeuui <jyj970328@gmail.com>
2024-01-19 17:27:08 +0900

    organization

--- 1106/review/17427.py
+++
@@ -1,8 +0,0 @@
-if __name__=='__main__':
-    n = int(input())
-
-    ret = 0
-    for i in range(1, n+1):
-        ret += (n//i)*i
-
-    print(ret)
--- 1204/10974.py
+++
@@ -1,9 +0,0 @@
-import sys
-input = sys.stdin.readline
-
-from itertools import permutations
-
-if __name__=='__main__':
-    n = int(input())
-    for p in list(permutations(range(1, n+1))):
-        print(' '.join(list(map(str, p))))
