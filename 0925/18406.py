4bd818b7b13ef3206a34194bc4dfb495fe20dfa5
zzeuui <jyj970328@gmail.com>
2024-01-19 17:26:03 +0900

    organization

--- 0925/18406.py
+++
@@ -1,8 +0,0 @@
-if __name__=='__main__':
-    n = list(map(int, list(input())))
-    
-    idx = len(n)//2
-    if sum(n[:idx]) == sum(n[idx:]):
-        print('LUCKY')
-    else:
-        print('READY')
