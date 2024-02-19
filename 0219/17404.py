fd459d6d5bb816739c75d905789987eb23fba2a8
zzeuui <jyj970328@gmail.com>
2024-02-19 21:21:27 +0900

    8th

---
+++ 0219/17404.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+house_rgb = [list(map(int, input().split())) for _ in range(n)]
+
+ans=1e9
+
+for i in range(3):
+    dp = [[ans, ans, ans] for _ in range(n)]
+    dp[0][i] = house_rgb[0][i]
+    for j in range(1, n):
+        dp[j][0] = house_rgb[j][0] + min(dp[j-1][1], dp[j-1][2])
+        dp[j][1] = house_rgb[j][1] + min(dp[j-1][0], dp[j-1][2])
+        dp[j][2] = house_rgb[j][2] + min(dp[j-1][0], dp[j-1][1])
+
+    for k in range(3):
+        if i != k: #1th house should not be same as n-th house
+            ans = min(ans, dp[-1][k])
+
+print(ans)
